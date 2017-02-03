0e6ec799bd741fbba114c5afe9c0cfb7e267d12e
langzhigang <zhigang.lang@happylifeplat.com>
2017-02-03 11:57:49 +0800

    编写一个restful api单元测试demo

---
+++ src/main/java/cn/lzg/springboot/background/demo/domain/User.java
@@ -0,0 +1,36 @@
+package cn.lzg.springboot.background.demo.domain;
+
+/**
+ * @author lzg
+ * @Date 2017/2/3.
+ */
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/test/java/cn/lzg/springboot/background/demo/controller/UserControllerTest.java
@@ -0,0 +1,96 @@
+package cn.lzg.springboot.background.demo.controller;
+
+import cn.lzg.springboot.background.demo.domain.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+/**
+ * @author lzg
+ * @Date 2017/2/3.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
+public class UserControllerTest {
+
+    private MockMvc mvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
+    @Test
+    public void testUserController() throws Exception {
+        // 测试UserController
+        RequestBuilder request;
+
+        // 1、get查一下user列表，应该为空
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        // 2、post提交一个user
+        User user = new User();
+        user.setId(1L);
+        user.setName("测试新增");
+        user.setAge(20);
+        String userJson = objectMapper.writeValueAsString(user);
+
+        request = post("/users/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson);
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 3、get获取user列表，应该有刚才插入的数据
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("["+userJson+"]")));
+
+        // 4、put修改id为1的user
+        user.setId(1L);
+        user.setName("测试修改");
+        user.setAge(30);
+        userJson = objectMapper.writeValueAsString(user);
+
+        request = put("/users/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson);
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 5、get一个id为1的user
+        request = get("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo(userJson)));
+
+        // 6、del删除id为1的user
+        request = delete("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
+
+        // 7、get查一下user列表，应该为空
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+    }
+}
