82b98c9bbb3b618d79c3f1d45864afdaff80c080
langzhigang <hunanlzg@qq.com>
2017-02-04 09:26:22 +0800

    添加jdbcTemplate支持

---
+++ src/main/java/cn/lzg/springboot/background/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package cn.lzg.springboot.background.demo.service.impl;
+
+import cn.lzg.springboot.background.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lzg
+ * @Date 2017/2/3.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void create(String name, Integer age) {
+        jdbcTemplate.update("insert into user(name, age) values(?, ?)", name, age);
+    }
+
+    @Override
+    public void deleteByName(String name) {
+        jdbcTemplate.update("delete from user where name = ?", name);
+    }
+
+    @Override
+    public Integer getAllUsers() {
+        return jdbcTemplate.queryForObject("select count(1) from user", Integer.class);
+    }
+
+    @Override
+    public void deleteAllUsers() {
+        jdbcTemplate.update("delete from user");
+    }
+}
---
+++ src/test/java/cn/lzg/springboot/background/demo/service/impl/UserServiceImplTest.java
@@ -0,0 +1,49 @@
+package cn.lzg.springboot.background.demo.service.impl;
+
+import cn.lzg.springboot.background.Application;
+import cn.lzg.springboot.background.demo.service.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author lzg
+ * @Date 2017/2/3.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class UserServiceImplTest {
+    @Autowired
+    private UserService userSerivce;
+
+    @Before
+    public void setUp() {
+        // 准备，清空user表
+        userSerivce.deleteAllUsers();
+    }
+
+    @Test
+    public void test() throws Exception {
+        // 插入5个用户
+        userSerivce.create("a", 1);
+        userSerivce.create("张三", 2);
+        userSerivce.create("c", 3);
+        userSerivce.create("d", 4);
+        userSerivce.create("e", 5);
+
+        // 查数据库，应该有5个用户
+        Assert.assertEquals(5, userSerivce.getAllUsers().intValue());
+
+        // 删除两个用户
+        userSerivce.deleteByName("a");
+        userSerivce.deleteByName("e");
+
+        // 查数据库，应该有5个用户
+        Assert.assertEquals(3, userSerivce.getAllUsers().intValue());
+
+    }
+}
