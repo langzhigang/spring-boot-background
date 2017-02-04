efbbd015063497feb6e7dc7e56b844ea216df9bb
langzhigang <zhigang.lang@happylifeplat.com>
2017-02-04 09:26:22 +0800

    添加jdbcTemplate支持

---
+++ src/main/java/cn/lzg/springboot/background/demo/service/UserService.java
@@ -0,0 +1,32 @@
+package cn.lzg.springboot.background.demo.service;
+
+/**
+ * @author lzg
+ * @Date 2017/2/3.
+ */
+public interface UserService {
+
+    /**
+     * 新增一个用户
+     * @param name
+     * @param age
+     */
+    void create(String name, Integer age);
+
+    /**
+     * 根据name删除一个用户高
+     * @param name
+     */
+    void deleteByName(String name);
+
+    /**
+     * 获取用户总量
+     */
+    Integer getAllUsers();
+
+    /**
+     * 删除所有用户
+     */
+    void deleteAllUsers();
+
+}
