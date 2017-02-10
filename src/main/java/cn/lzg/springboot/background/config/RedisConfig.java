f60f39291cfc0a40ab1fd626530fbae614e56693
langzhigang <hunanlzg@qq.com>
2017-02-10 16:32:38 +0800

    引入redis

---
+++ src/main/java/cn/lzg/springboot/background/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package cn.lzg.springboot.background.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author lzg
+ * @Date 2017/2/9.
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis")
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory,
+                                                       @Qualifier("jedisConnectionFactory") JedisConnectionFactory jedisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        return template;
+    }
+
+
+}
